a1e62fa3d17846c9255526a4a83f533d28040053
Renan D. Gava <renandellecrodegava7040@gmail.com>
2022-03-24 21:05:10 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SinginButton } from '../SinginButton'
+import styles from './styles.module.scss'
+
+export function Header() {
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+
+                <SinginButton />
+            </div>
+        </header>
+    )
+}
